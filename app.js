0a2a5da488ddae0950f41d61c1f04114085cab7c
kolamat <matanmidupe004@gmail.com>
2024-01-19 14:42:34 +0100

    Changes to be committed: 	new file:   anime.es.js 	new file:   anime.js 	new file:   anime.min.js 	modified:   app.js 	modified:   index.html

--- app.js
+++ app.js
@@ -3,3 +3,15 @@ const Logo = document.querySelectorAll("#logo path");
 for (let i = 0; i < Logo.length; i++) {
   console.log(`Letter ${i} is ${Logo[i].getTotalLength()}`);
 }
+
+anime({
+  targets: "#seclogo path",
+  strokeDashoffset: [anime.setDashoffset, 0],
+  easing: "easeInOutSine",
+  duration: 1500,
+  delay: function (el, i) {
+    return i * 250;
+  },
+  direction: "alternate",
+  loop: true,
+});
