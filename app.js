92a0fa91cb186ecece28ad90746237e578a8b1a0
kolamat <matanmidupe004@gmail.com>
2024-01-19 14:47:05 +0100

    On branch main Your branch is up to date with 'origin/main'.
    Changes to be committed:
    	modified:   app.js

--- app.js
+++ app.js
@@ -8,11 +8,10 @@ anime({
   targets: "#seclogo path",
   strokeDashoffset: [anime.setDashoffset, 0],
   easing: "easeInOutSine",
-  fill: [rgb(96, 165, 250)],
   duration: 1500,
   delay: function (el, i) {
     return i * 250;
   },
   direction: "alternate",
-  loop: false,
+  loop: true,
 });
